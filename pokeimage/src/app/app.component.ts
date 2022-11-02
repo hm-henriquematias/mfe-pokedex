0273f8e99383a60af7372c8c7b385a2bd7d00c77
Henrique Matias <henriquematiasdesouza@gmail.com>
2022-11-02 17:02:29 -0300

    pokeimage; feature: Clean html and convert to serve as file to be used as component

--- pokeimage/src/app/app.component.ts
+++ pokeimage/src/app/app.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: []
 })
 export class AppComponent {
+  sprite = 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/132.png';
   title = 'pokeimage';
 }
--- pokeimage/src/app/app.module.ts
+++ pokeimage/src/app/app.module.ts
@@ -1,7 +1,6 @@
-import { NgModule } from '@angular/core';
+import { Injector, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { createCustomElement } from '@angular/elements';
 import { AppComponent } from './app.component';
 
 @NgModule({
@@ -9,10 +8,17 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: []
 })
-export class AppModule { }
+
+export class AppModule {
+  constructor(private injector: Injector) { }
+
+  ngDoBootstrap() {
+    const custom = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('angular-pokeimage', custom);
+  }
+ }
