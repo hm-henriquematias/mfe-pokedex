2b79f53501ad23492c24bfe2b8d884a640e1c007
Henrique Matias <henriquematiasdesouza@gmail.com>
2022-11-02 10:18:31 -0300

    pokedex;feature: add Lazy load in app.module.ts

--- pokedex/src/app/app.module.ts
+++ pokedex/src/app/app.module.ts
@@ -1,16 +1,21 @@
-import { NgModule } from '@angular/core';
+import { LazyElementsModule } from '@angular-extensions/elements';
+import { CommonModule } from '@angular/common';
+import { CUSTOM_ELEMENTS_SCHEMA, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   declarations: [
     AppComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LazyElementsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
